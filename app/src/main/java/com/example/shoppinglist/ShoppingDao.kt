d9ca2097d4652c43cfe6dd0fe03a9a236451b752
saiful islam <saifulislambaust28@gmail.com>
2022-03-22 14:14:54 +0600

    start project some modification 1
    Signed-off-by: saiful islam <saifulislambaust28@gmail.com>

---
+++ app/src/main/java/com/example/shoppinglist/ShoppingDao.kt
@@ -0,0 +1,12 @@
+package com.example.shoppinglist
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+
+@Dao
+class ShoppingDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun upsert(item:ShoppingItem)
+}
